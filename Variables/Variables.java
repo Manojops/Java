813aabc8d18357b0e7933bf057fe76f63ce584f4
Manojops <manojboy562@gmail.com>
2025-09-30 11:20:31 -0700

    Variables

---
+++ Variables/Variables.java
@@ -0,0 +1,10 @@
+public class Variables{
+    public static void main(String[] args){
+        int num1=7;  //Creating a variable num1 stores integer type
+        int num2=10;
+        int result=num1+num2;  //store addition of two numbers 
+
+        System.out.println("Result : "+result);
+        //Display the result variable (addition of two numbers)
+    }
+}
