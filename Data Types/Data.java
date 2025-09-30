2c756b66f7999d711d955e7f8fcbcf58eab59352
Manojops <manojboy562@gmail.com>
2025-09-30 11:50:00 -0700

    Data types in java

---
+++ Data Types/Data.java
@@ -0,0 +1,15 @@
+public class Data{
+    public static void main(String[] args) {
+        byte b = 120;
+        int num = 8000;
+        short sh = 123;
+        long lg = 120000l;
+
+        float f = 2.55f;
+        double db = 1.53332;
+
+        char gender = 'M';
+
+        boolean isAdult = true;
+    }
+}
