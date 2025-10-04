42fce600b9809875d0128cf78aefd5bc0030374d
Manojops <manojboy562@gmail.com>
2025-10-04 15:52:29 +0530

    Problems in Oops

---
+++ 10 - Interface/src/Main.java
@@ -0,0 +1,31 @@
+
+interface Shape{
+    void area();
+    void display();
+}
+
+class Triangle implements Shape{
+    public void area(){
+        //------------
+    }
+
+    public void display(){
+        //------------
+    }
+}
+
+class Rectangle implements Shape{
+    public void area(){
+        //---------------
+    }
+
+    public void display(){
+        //---------------
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        //----------------
+    }
+}
